5a7a8405e53c265e0b18209e43411f8129a9f43c
pjanec <pjanec@hotmail.com>
2026-05-21 09:11:43 +0200

    fix: BlueprintBlackboard1024 constant

--- FDP/Toolkits/Fdp.Toolkits/Blueprints/Components/BlueprintBlackboard1024.cs
+++ FDP/Toolkits/Fdp.Toolkits/Blueprints/Components/BlueprintBlackboard1024.cs
@@ -8,7 +8,7 @@ namespace Fdp.Toolkit.Blueprints.Components;
 /// Component ID 205 reserved for Blueprint subsystem.
 /// </summary>
 [StructLayout(LayoutKind.Sequential, Size = 1024)]
-[ComponentId(205)]
+[ComponentId(GlobalComponentIds.BlueprintBlackboard1024)]
 public unsafe struct BlueprintBlackboard1024
 {
     public fixed byte Data[1024];
