0d0f7504804381f08872b7d6690645560931a5dc
pjanec <pjanec@hotmail.com>
2026-05-29 23:59:02 +0200

    chore: dump to 4 files

--- dump.bat
+++ dump.bat
@@ -1,6 +1,11 @@
 ::python C:\Utils\AITools\CodeDump\dump.py --debug --dry-run --filter-file just-src.dumpfilter . fdpdump.txt 
-python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-no-ext-FDP.dumpfilter . FDP.txt 
-python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-no-ext-HROT.dumpfilter . HROT.txt 
+python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-EXT.dumpfilter . EXT.txt 
+python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-FDP.dumpfilter . FDP.txt 
+python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-HROT.Engine.dumpfilter . HROT.Eng.txt 
+python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-HROT.Subsys.dumpfilter . HROT.Sub.txt 
+
 ::python C:\Utils\AITools\CodeDump\dump.py --filter-file just-tests-no-ext-FDP.dumpfilter . FDP-Tests.txt 
 ::python C:\Utils\AITools\CodeDump\dump.py --filter-file just-tests-no-ext-HROT.dumpfilter . HROT-Tests.txt 
-python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-ext-NodeEdit.dumpfilter . NodeEdit.txt 
+::python C:\Utils\AITools\CodeDump\dump.py --filter-file no-tests-ext-NodeEdit.dumpfilter . NodeEdit.txt 
+
+
